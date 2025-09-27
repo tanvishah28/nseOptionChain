74b52d0ce8207bd22ec01373ea871525167c20ed
Tanvi <tanvishah280@gmail.com>
2025-09-27 15:21:53 +0530

    Fixed:Updated code for page setUserAgent

--- nseOptionChainExpress.js
+++ nseOptionChainExpress.js
@@ -6,17 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/option-chain", async (req, res) => {
   const symbol = req.query.symbol || "NATIONALUM";
-
   let browser = null;
 
   try {
     browser = await chromium.puppeteer.launch({
       args: chromium.args,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath || null,
       headless: chromium.headless,
     });
 
     const page = await browser.newPage();
+
+    // Spoof user-agent to bypass NSE bot detection
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    );
+
     await page.goto(`https://www.nseindia.com/option-chain?symbol=${symbol}`, {
       waitUntil: "networkidle2",
     });
